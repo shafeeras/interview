dc2fd8bed59352fbb51e0aa07b8b45b427112614
shafeeras <shafeeras1509@gmail.com>
2020-03-29 20:43:44 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const path = require('path')
+const cors = require("cors");
 app.disable('x-powered-by')
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true
+};
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, 'build')))
 // need to declare a "catch all" route on your express server
 // that captures all page requests and directs them to the client
--- src/services/MovieList/index.js
+++ src/services/MovieList/index.js
@@ -4,6 +4,7 @@ import { updateMovies } from "../../actions/Movies";
 
 export const fetchMovies = offSet => (dispatch, getState) => {
   return API.get(`${endPoints.movieList}${offSet}.JSON`).then(resp => {
+    console.log(resp)
     dispatch(updateMovies({ ...resp.data.page, offSet }));
   }).catch(error => {
     // error nedd to handle
