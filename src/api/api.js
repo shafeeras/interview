d33515546895cc2196740b14e7ba8d4634da0d7d
shafeeras <shafeeras1509@gmail.com>
2020-03-29 19:27:57 +0530

    changes

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { onResponse, onResponseError } from "./responseInterceptor";
 import { onRequest, onRequestError } from "./requestInterceptor";
 
-export const BASE_URL = "https://shafeeras.github.io";
+export const BASE_URL = "http://localhost:5000";
 
 export const API = axios.create({
   baseURL: BASE_URL
